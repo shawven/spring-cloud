d07bd4f2932997cc46fd72e026f749e61e092a37
shoven1 <441653192@qq.com>
2018-12-14 17:59:12 +0800

    commit

--- modules/client1/src/main/java/com/sc/client/controllers/IndexController.java
+++ modules/client1/src/main/java/com/sc/client/controllers/IndexController.java
@@ -1,6 +1,7 @@
 package com.sc.client.controllers;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,22 +10,23 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2018-12-13
  */
 @RestController
+@RefreshScope
 public class IndexController {
 
-    @Value("${server.port}")
+    @Value("${server.port}" )
     private String port;
 
-//    @Value("${hello}")
-//    private String hello;
+    @Value("${hello}")
+    private String hello;
 
     @RequestMapping("/")
     public String index() {
         return "hello world from port " + port;
     }
 
-//    @RequestMapping("hello")
-//    public String hello() {
-//        return hello;
-//    }
+    @RequestMapping("hello")
+    public String hello() {
+        return hello;
+    }
 
 }
