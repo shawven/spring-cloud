5a9c895013b03015c671924b19954557244ba111
shoven1 <441653192@qq.com>
2018-12-26 17:52:04 +0800

    更多组件

--- gateway/src/main/java/com/sc/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/sc/gateway/GatewayApplication.java
@@ -2,8 +2,12 @@ package com.sc.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@SpringBootApplication
+@EnableZuulProxy
+@SpringCloudApplication
 public class GatewayApplication {
 
     public static void main(String[] args) {
--- modules/client1/src/main/java/com/sc/client/ClientApplication.java
+++ modules/client1/src/main/java/com/sc/client/ClientApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ClientApplication {
